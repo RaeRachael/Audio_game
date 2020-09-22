584c0c06e570e4115974dc6cd96f5553d973bdcc
Rachael Ewins <rachaelewins@hotmail.com>
2020-09-22 17:19:09 +0100

    Next tile is made in the right location

--- __tests__/levelGeneration.ts
+++ __tests__/levelGeneration.ts
@@ -1,4 +1,4 @@
-import { createLevel } from "../src/level"
+import { createLevel, createSingleTile } from "../src/level"
 
 describe( "function createLevel()", function() {
 
@@ -19,6 +19,87 @@ describe( "function createLevel()", function() {
     expect( typeof createLevel(1)[0].paths.South === 'boolean').toBeTruthy()
     expect( typeof createLevel(1)[0].paths.East === 'boolean').toBeTruthy()
     expect( typeof createLevel(1)[0].paths.West === 'boolean').toBeTruthy()
+
+  })
+
+  it( "the first tile is at '0,0' and has one availiable path", function() {
+
+    var firstTile = createLevel(1)[0]
+    var availiablePaths = 0
+    if (firstTile.paths.North) availiablePaths++
+    if (firstTile.paths.South) availiablePaths++
+    if (firstTile.paths.East) availiablePaths++
+    if (firstTile.paths.West) availiablePaths++
+
+    expect(firstTile.position).toEqual({x: 0, y: 0})
+    
+    expect(availiablePaths).toEqual(1)
+    
+  })
+
+  it( "the second tile is not at '0,0' ", function() {
+
+    var secondTile = createLevel(2)[1]
+    // var availiablePaths = 0
+    // if (firstTile.paths.North) availiablePaths++
+    // if (firstTile.paths.South) availiablePaths++
+    // if (firstTile.paths.East) availiablePaths++
+    // if (firstTile.paths.West) availiablePaths++
+
+    expect(secondTile.position).not.toEqual({x: 0, y: 0})
+    
+    // expect(availiablePaths).toEqual(1)
+    
+  })
+
+})
+
+describe( "function create singleTile(previousTile)", function() {
+  describe( "created tile is in the correct position based on the openPath", function() {
+
+    it( "the second tile is at '0,1' if openPath is 'North'", function() {
+      var previousTile = {
+        "position": {"x": 0, "y": 0},
+        "paths": { "North": true, "South": false, "East": false, "West": false},
+        "openPaths": {"North": true, "South": false, "East": false, "West": false}
+      }
+      var secondTile = createSingleTile(previousTile)
+  
+      expect(secondTile.position).toEqual({x: 0, y: 1})
+    })
+
+    it( "the second tile is at '0,-1' if openPath is 'South'", function() {
+      var previousTile = {
+        "position": {"x": 0, "y": 0},
+        "paths": { "North": true, "South": false, "East": false, "West": false},
+        "openPaths": {"North": false, "South": true, "East": false, "West": false}
+      }
+      var secondTile = createSingleTile(previousTile)
+  
+      expect(secondTile.position).toEqual({x: 0, y: -1}) 
+    })
+
+    it( "the second tile is at '1,0' if openPath is 'East'", function() {
+      var previousTile = {
+        "position": {"x": 0, "y": 0},
+        "paths": { "North": true, "South": false, "East": false, "West": false},
+        "openPaths": {"North": false, "South": false, "East": true, "West": false}
+      }
+      var secondTile = createSingleTile(previousTile)
+  
+      expect(secondTile.position).toEqual({x: 1, y: 0}) 
+    })
+
+    it( "the second tile is at '-1,0' if openPath is 'West'", function() {
+      var previousTile = {
+        "position": {"x": 0, "y": 0},
+        "paths": { "North": true, "South": false, "East": false, "West": false},
+        "openPaths": {"North": false, "South": false, "East": false, "West": true}
+      }
+      var secondTile = createSingleTile(previousTile)
+
+      expect(secondTile.position).toEqual({x: -1, y: 0})
+    })
 
   })
 
--- src/level.ts
+++ src/level.ts
@@ -1,13 +1,38 @@
+type Tile = {
+  position: {x: number, y:number}
+  paths: {"North": boolean, "South": boolean, "East": boolean, "West": boolean}
+  openPaths: {"North": boolean, "South": boolean, "East": boolean, "West": boolean}
+}
+
 export function createLevel(number) {
   var i:number;
-  type Tile = {
-    position: {x: number, y:number};
-    paths: {"North": boolean, "South": boolean, "East": boolean, "West": boolean};
+  var levelMap: Tile[] = [ {
+    "position": {"x": 0, "y": 0},
+    "paths": { "North": true, "South": false, "East": false, "West": false},
+    "openPaths": {"North": true, "South": false, "East": false, "West": false}
+  } ]
+  for ( i = 0; i < number**2 - 1; i++ ) {
+    levelMap.push(createSingleTile(levelMap[i]))
   }
-  var levelMap: Tile[] = []
-
-  for ( i = 0; i < number**2; i++ ) {
-    levelMap.push({ "position": {"x": 1, "y": 1} , "paths": { "North": true, "South": false, "East": false, "West": false} })
-  }
+  console.log(levelMap)
   return levelMap
+}
+
+export function createSingleTile(previousTile) {
+  var singleTile:Tile = {
+    "position": {"x": 0, "y": 0},
+    "paths": { "North": true, "South": false, "East": false, "West": false},
+    "openPaths": {"North": true, "South": false, "East": false, "West": false}
+  }
+  var openPathValue: { x:number, y:number } = {x: 0, y: 0}
+  if (previousTile.openPaths.North) openPathValue.y = 1
+  if (previousTile.openPaths.South) openPathValue.y = -1
+  if (previousTile.openPaths.East) openPathValue.x = 1
+  if (previousTile.openPaths.West) openPathValue.x = -1
+  singleTile.position.x = previousTile.position.x + openPathValue.x
+  singleTile.position.y = previousTile.position.y + openPathValue.y
+  singleTile.paths = { "North": true, "South": false, "East": false, "West": false}
+  singleTile.openPaths = { "North": true, "South": false, "East": false, "West": false}
+
+  return singleTile
 }
