84118a8b5b175dbece3200d98ac65c40821adebd
Rachael Ewins <rachaelewins@hotmail.com>
2020-09-25 14:32:33 +0100

    Refactored if blocks into a loop block

--- src/level.ts
+++ src/level.ts
@@ -1,15 +1,22 @@
+type Position = {x: number, y: number}
 type Tile = {
-  position: {x: number, y:number}
+  position: Position
   paths: {"North": boolean, "South": boolean, "East": boolean, "West": boolean}
   openPaths: {"North": boolean, "South": boolean, "East": boolean, "West": boolean}
   numberOpenPaths: number
 }
+type DirectionValues = {"North": Position, "South": Position, "East": Position, "West": Position}
+
+const directionValues:DirectionValues = { 
+  "North": {x: 0, y: 1}, "South": {x: 0, y: -1}, "East": {x: 1, y: 0}, "West": {x: -1, y: 0} 
+}
+
+var i:number;
 
 export function createLevel(number) {
-  var i:number;
   var levelMap: Tile[] = [ {
     "position": {"x": 0, "y": 0},
-    "paths": { "North": true, "South": false, "East": false, "West": false},
+    "paths": {"North": true, "South": false, "East": false, "West": false},
     "openPaths": {"North": true, "South": false, "East": false, "West": false},
     "numberOpenPaths": 1
   } ]
@@ -23,32 +30,25 @@ export function createLevel(number) {
 export function createSingleTile(previousTile) {
   var singleTile:Tile = {
     "position": {"x": 0, "y": 0},
-    "paths": { "North": true, "South": false, "East": false, "West": false},
+    "paths": {"North": true, "South": false, "East": false, "West": false},
     "openPaths": {"North": true, "South": false, "East": false, "West": false},
     "numberOpenPaths": 1
   }
   var openPathValue: { x:number, y:number } = {x: 0, y: 0}
-  if (previousTile.openPaths.North) {
-    openPathValue.y = 1
-    previousTile.openPaths.North = false
+
+  for (let direction in previousTile.openPaths) {
+    if (previousTile.openPaths[direction]) {
+      openPathValue.x = directionValues[direction].x
+      openPathValue.y = directionValues[direction].y
+      previousTile.openPaths[direction] = false
+      break
+    }
   }
-  else if (previousTile.openPaths.South) {
-    openPathValue.y = -1
-    previousTile.openPaths.South = false
-  }
-  else if (previousTile.openPaths.East) {
-    openPathValue.x = 1
-    previousTile.openPaths.East = false
-  }
-  else if (previousTile.openPaths.West) {
-    openPathValue.x = -1
-    previousTile.openPaths.west = false
-  }
-  // console.log(previousTile.position, openPathValue)
+  
   singleTile.position.x = previousTile.position.x + openPathValue.x
   singleTile.position.y = previousTile.position.y + openPathValue.y
-  singleTile.paths = { "North": true, "South": false, "East": false, "West": false}
-  singleTile.openPaths = { "North": true, "South": false, "East": false, "West": false}
+  singleTile.paths = {"North": true, "South": false, "East": false, "West": false}
+  singleTile.openPaths = {"North": true, "South": false, "East": false, "West": false}
 
   previousTile.numberOpenPaths--
 
