de69a4c4c6d77a86beafb6eccf95340e2ef09107
Rachael Ewins <rachaelewins@hotmail.com>
2020-11-17 15:28:15 +0000

    tests fixed

--- dist/__tests__/player/player.js
+++ dist/__tests__/player/player.js
@@ -1,5 +1,6 @@
 import { Audio } from "../../src/audio/audio";
 import { Level } from "../../src/level/level";
+import { displayText } from "../../src/main";
 import { Player } from "../../src/player/player";
 jest.mock("../../src/audio/audio", () => ({
     Audio: jest.fn().mockImplementation(() => {
@@ -9,12 +10,37 @@ jest.mock("../../src/audio/audio", () => ({
         };
     })
 }));
+jest.mock("../../src/main", () => ({ displayText: jest.fn() }));
 var levelMap = [{
         "position": { "x": 0, "y": 0 },
-        "paths": { "North": false, "South": false, "East": false, "West": false },
+        "paths": { "North": true, "South": false, "East": true, "West": true },
         "openPaths": { "North": false, "South": false, "East": false, "West": false },
         "numberOpenPaths": 0,
         "exitTile": false
+    }, {
+        "position": { "x": 0, "y": 1 },
+        "paths": { "North": true, "South": true, "East": false, "West": false },
+        "openPaths": { "North": false, "South": false, "East": false, "West": false },
+        "numberOpenPaths": 0,
+        "exitTile": false
+    }, {
+        "position": { "x": -1, "y": 0 },
+        "paths": { "North": false, "South": false, "East": true, "West": false },
+        "openPaths": { "North": false, "South": false, "East": false, "West": false },
+        "numberOpenPaths": 0,
+        "exitTile": false
+    }, {
+        "position": { "x": 1, "y": 0 },
+        "paths": { "North": false, "South": false, "East": false, "West": true },
+        "openPaths": { "North": false, "South": false, "East": false, "West": false },
+        "numberOpenPaths": 0,
+        "exitTile": false
+    }, {
+        "position": { "x": 0, "y": 2 },
+        "paths": { "North": false, "South": true, "East": false, "West": false },
+        "openPaths": { "North": false, "South": false, "East": false, "West": false },
+        "numberOpenPaths": 0,
+        "exitTile": true
     }];
 var mockedlevel = new Level(levelMap);
 var player = new Player;
@@ -32,18 +58,15 @@ describe("Player()", function () {
     });
     describe("when unblocked", function () {
         it("have steps in the direction facing", function () {
-            jest.spyOn(mockedlevel, 'blockingDistance').mockReturnValue(1);
             player.step();
             expect(player.position).toEqual({ x: 0, y: 1 });
         });
         it("have steps in the direction facing", function () {
-            jest.spyOn(mockedlevel, 'blockingDistance').mockReturnValue(1);
             player.left();
             player.step();
             expect(player.position).toEqual({ x: -1, y: 0 });
         });
         it("have steps in the direction facing", function () {
-            jest.spyOn(mockedlevel, 'blockingDistance').mockReturnValue(1);
             player.right();
             player.step();
             expect(player.position).toEqual({ x: 1, y: 0 });
@@ -51,9 +74,17 @@ describe("Player()", function () {
     });
     describe("when blocked", function () {
         it("does not step forward", function () {
-            jest.spyOn(mockedlevel, 'blockingDistance').mockReturnValue(0);
+            player.left();
+            player.left();
             player.step();
             expect(player.position).toEqual({ x: 0, y: 0 });
+        });
+    });
+    describe("calls displayText if on exit tile", function () {
+        it("does not step forward", function () {
+            player.step();
+            player.step();
+            expect(displayText).toBeCalledTimes(1);
         });
     });
 });
